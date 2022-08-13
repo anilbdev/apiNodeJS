da22b22ff7b30d273f7fec9ddb4876923638550f
bhasi <anilbhasib@gmail.com>
2022-08-13 13:39:54 +0530

    original

--- apiSchema/categorySchema.js
+++
@@ -1,6 +0,0 @@
-const Joi = require('joi')
-module.exports.getCategorySchema = Joi.object({
-    name:Joi.string().required(),
-    brand:Joi.string().required(),
-    price:Joi.number().required(),
-})
